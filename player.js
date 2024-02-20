c6f5bbb17db6a18594db9a7c4b4d00162d19c006
Franklin Oliveira <franklin.oliveira@assesso.com.br>
2024-02-20 10:01:05 -0300

    ajustes player da sessao Toques

--- player.js
+++ player.js
@@ -5,62 +5,64 @@ var slide = document.querySelector('.slide input[type="range"]');
 
 var audioSeek = document.getElementById("audioSrc");
 
-range.oninput = function() {
-  completionBar.style.width = this.value+'%';
-  audioSeek.currentTime = ((this.value/100))*audioSeek.duration;
+range.oninput = function () {
+  completionBar.style.width = this.value + '%';
+  audioSeek.currentTime = ((this.value / 100)) * audioSeek.duration;
 }
 
 
 //------------------------------------------
 
-function play(element)
-{
-  
-  var nowPlayingBoardId = document.getElementById("now-playing-board-id"); 
-  var nowPlayingBoardBottomBarId = document.getElementById("now-playing-board-bottom-bar-id");  
-  
-   var vynlId = document.getElementById("vynl-id"); 
-  
-  var audio = document.getElementById("audioSrc");
-  
-  audio.loop = true;
-  
-  // Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
-audio.ontimeupdate = function() {
-  completionBar.style.width = ((this.currentTime/this.duration)*100)+'%';
-  if(completionBar.style.width > "99%")
-  {
-    range.value=0;  
-  }
-  else
-  {
-    console.log(' '+completionBar.style.width);
-  }
-};
-  if(document.getElementById("playpause").classList.contains("play-circle"))
-  {  
-vynlId.classList.add("vynl-animation");
-    
- //document.getElementById("audioSrc").play();
- tocar(document.querySelector("#toque_angola_batidas").value)
+function play(element) {
 
-     nowPlayingBoardId.style.transform="translatey(-30%)";
-     nowPlayingBoardBottomBarId.style.transform="translatey(0)";
-    
+  var nowPlayingBoardId = document.getElementById("now-playing-board-id");
+  var nowPlayingBoardBottomBarId = document.getElementById("now-playing-board-bottom-bar-id");
+
+  var vynlId = document.getElementById("vynl-id");
+
+  var audio = document.getElementById("audioSrc");
+
+  audio.loop = true;
+
+  // Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
+  audio.ontimeupdate = function () {
+    completionBar.style.width = ((this.currentTime / this.duration) * 100) + '%';
+    if (completionBar.style.width > "99%") {
+      range.value = 0;
+    }
+    else {
+      console.log(' ' + completionBar.style.width);
+    }
+  };
+  if (isPlay()) {
+    vynlId.classList.add("vynl-animation");
+
+    //document.getElementById("audioSrc").play();
+    tocar("0.5s tchi 0.5s tchi 0.5s domchi 1.25s dim")
+
+    nowPlayingBoardId.style.transform = "translatey(-30%)";
+    nowPlayingBoardBottomBarId.style.transform = "translatey(0)";
+
     document.getElementById("playpause").classList.remove("play-circle");
     document.getElementById("playpause").classList.add("pause-circle");
   }
-   else if(document.getElementById("playpause").classList.contains("pause-circle"))
-  {    
-   vynlId.classList.remove("vynl-animation");
-   
-  //document.getElementById("audioSrc").pause(); 
-  document.querySelectorAll(".parar").forEach(pararAudio => pararAudio.addEventListener("click", parar))
-    nowPlayingBoardId.style.transform="translatey(56%)";
-    nowPlayingBoardBottomBarId.style.transform="translatey(-20%)";
-    
+  else if (isPause()) {
+    vynlId.classList.remove("vynl-animation");
+
+    //document.getElementById("audioSrc").pause(); 
+    document.querySelectorAll(".parar").forEach(pararAudio => pararAudio.addEventListener("click", parar))
+    nowPlayingBoardId.style.transform = "translatey(56%)";
+    nowPlayingBoardBottomBarId.style.transform = "translatey(-20%)";
+
     document.getElementById("playpause").classList.remove("pause-circle");
     document.getElementById("playpause").classList.add("play-circle");
   }
 }
 
+function isPlay() {  
+  return document.getElementById("playpause").classList.contains("play-circle")
+}
+
+function isPause() {  
+  return document.getElementById("playpause").classList.contains("pause-circle")
+}
--- scripts.js
+++ scripts.js
@@ -1,140 +1,164 @@
 let velocidade = 1
-        let idsReproducao = [];
+let idsReproducao = [];
 
-        //document.querySelector("#velocidade").textContent = velocidade + "x"
+//document.querySelector("#velocidade").textContent = velocidade + "x"
 
-        document.querySelectorAll("[data-toque]").forEach(toque => {
-            let audio = new Audio(`audio/${toque.dataset.toque}.mp3`)
-            audio.autoPlay = false
-            toque.addEventListener("click", function(event) {
-                if (event.target.dataset.toque == "dom") {
-                    tocarDom()
-                }
-                if (event.target.dataset.toque == "tchi") {
-                    tocarTchi()
-                }
-                if (event.target.dataset.toque == "dim") {
-                    tocarDim()
-                }
-                if (event.target.dataset.toque == "domchi") {
-                    tocarDomChi()
-                }
-                event.target.value = ""
-            })
-        })
+document.querySelectorAll("[data-toque]").forEach(toque => {
+    let audio = new Audio(`audio/${toque.dataset.toque}.mp3`)
+    audio.autoPlay = false
+    toque.addEventListener("click", function(event) {
+        if (event.target.dataset.toque == "dom") {
+            tocarDom()
+        }
+        if (event.target.dataset.toque == "tchi") {
+            tocarTchi()
+        }
+        if (event.target.dataset.toque == "dim") {
+            tocarDim()
+        }
+        if (event.target.dataset.toque == "domchi") {
+            tocarDomChi()
+        }
+        event.target.value = ""
+    })
+})
 
+//document.querySelector("#toque_personalizado").addEventListener("click", () => tocar(document.querySelector("#toque_personalizado_batidas").value))
+//document.querySelector("#toque_angola").addEventListener("click", () => tocar(document.querySelector("#toque_angola_batidas").value))
+//document.querySelector("#toque_sbp").addEventListener("click", () => tocar(document.querySelector("#toque_sbp_batidas").value))
+//document.querySelector("#toque_sbg").addEventListener("click", () => tocar(document.querySelector("#toque_sbg_batidas").value))
+//document.querySelector("#toque_angola2").addEventListener("click", () => tocar(document.querySelector("#toque_angola_batidas").value))
+//document.querySelector("#toque_sbp2").addEventListener("click", () => tocar(document.querySelector("#toque_sbp_batidas").value))
+//document.querySelector("#toque_sbg2").addEventListener("click", () => tocar(document.querySelector("#toque_sbg_batidas").value))
+//document.querySelector("#parar").addEventListener("click", parar)
+//document.querySelectorAll(".parar").forEach(pararAudio => pararAudio.addEventListener("click", parar))
+
+/*
+document.querySelector("#velocidade_mais").addEventListener("click", async function(event) {
+    velocidade += 0.25
+    document.querySelector("#velocidade").textContent = velocidade + "x"
+})
+
+document.querySelector("#velocidade_menos").addEventListener("click", async function(event) {
+    velocidade -= 0.25
+    document.querySelector("#velocidade").textContent = velocidade + "x"
+})*/
+
+function tocar(batidas) {
+    console.log("#################### " + batidas)
+    
+    let batidasPartes = batidas.split(" ")
+
+    let batidasValidas = true;
+    let msgValidacao = "";
+    batidasPartes.forEach(batida => {
+        if (batida != "dom" && batida != "dim" && batida != "tchi" && batida != "domchi" && !batida.includes("s")) {
+            batidasValidas = false;
+            msgValidacao += "Simbolo '" + batida + "'' invalido. "
+        }
+    })
+
+    if (!batidasValidas) {
+        alert(msgValidacao)
+        return
+    }
+    
+    let timeout = 0
+    let batidaParteIntervalo = 1
+    batidasPartes.forEach(batidaParte => {
+        if (batidaParte.includes("s")) {
+            batidaParteIntervalo = Number(batidaParte.replace("s", ""))                    
+        } else {
+            batidaParteIntervalo = 1
+        }
+
+        timeout += batidaParteIntervalo * 1000 / velocidade
         
+        if (batidaParte == "dom") {
+            idsReproducao.push(setTimeout(tocarDom, timeout))
+            batidaParteIntervalo = 1
+        } else if (batidaParte == "tchi") {
+            idsReproducao.push(setTimeout(tocarTchi, timeout))
+            batidaParteIntervalo = 1
+        } else if (batidaParte == "dim") {
+            idsReproducao.push(setTimeout(tocarDim, timeout))
+            batidaParteIntervalo = 1
+        } else if (batidaParte == "domchi") {
+            idsReproducao.push(setTimeout(tocarDomChi, timeout))
+            batidaParteIntervalo = 1
+        }
+    })
+    if (document.querySelector("#repetir") && document.querySelector("#repetir").checked) {
+        idsReproducao.push(setTimeout(() => tocar(batidas), timeout))
+    }            
+}
 
-        //document.querySelector("#toque_personalizado").addEventListener("click", () => tocar(document.querySelector("#toque_personalizado_batidas").value))
-        //document.querySelector("#toque_angola").addEventListener("click", () => tocar(document.querySelector("#toque_angola_batidas").value))
-        //document.querySelector("#toque_sbp").addEventListener("click", () => tocar(document.querySelector("#toque_sbp_batidas").value))
-        //document.querySelector("#toque_sbg").addEventListener("click", () => tocar(document.querySelector("#toque_sbg_batidas").value))
-        //document.querySelector("#toque_angola2").addEventListener("click", () => tocar(document.querySelector("#toque_angola_batidas").value))
-        //document.querySelector("#toque_sbp2").addEventListener("click", () => tocar(document.querySelector("#toque_sbp_batidas").value))
-        //document.querySelector("#toque_sbg2").addEventListener("click", () => tocar(document.querySelector("#toque_sbg_batidas").value))
-        //document.querySelector("#parar").addEventListener("click", parar)
-        //document.querySelectorAll(".parar").forEach(pararAudio => pararAudio.addEventListener("click", parar))
+function parar() {
+    idsReproducao.forEach(idReproducao => {
+        clearTimeout(idReproducao)
+    })
+    idsReproducao = []
+}
 
-        /*
-        document.querySelector("#velocidade_mais").addEventListener("click", async function(event) {
-            velocidade += 0.25
-            document.querySelector("#velocidade").textContent = velocidade + "x"
-        })
+function tocarTchi() {
+    new Audio("audio/tchi.mp3").play()
+}
 
-        document.querySelector("#velocidade_menos").addEventListener("click", async function(event) {
-            velocidade -= 0.25
-            document.querySelector("#velocidade").textContent = velocidade + "x"
-        })*/
+function tocarDomChi() {
+    new Audio("audio/domchi.mp3").play()
+}
 
-        function tocar(batidas) {
-            
-            let batidasPartes = batidas.split(" ")
+function tocarDim() {
+    new Audio("audio/dim.mp3").play()
+}
 
-            let batidasValidas = true;
-            let msgValidacao = "";
-            batidasPartes.forEach(batida => {
-                if (batida != "dom" && batida != "dim" && batida != "tchi" && batida != "domchi" && !batida.includes("s")) {
-                    batidasValidas = false;
-                    msgValidacao += "Simbolo '" + batida + "'' invalido. "
-                }
-            })
+function tocarDom() {
+    new Audio("audio/dom.mp3").play()
+}
 
-            if (!batidasValidas) {
-                alert(msgValidacao)
-                return
+document.querySelector("#compartilhar").addEventListener("click", async function(event) {
+    const response = await fetch('https://media.istockphoto.com/id/924098198/pt/vetorial/isolated-colorful-decorative-ornate-berimbau-with-caxixi-baqueta-and-dobrao-on-white.jpg?s=612x612&w=0&k=20&c=Tw57MSFN9X2-f-1PFnztcP2V9PLvO2HtlC36nFEzEPU=');
+    const blob = await response.blob();
+    const filesArray = [
+        new File(
+            [blob],
+            'berimbau.jpg',
+            {
+            type: "image/jpeg"
             }
-            
-            let timeout = 0
-            let batidaParteIntervalo = 1
-            batidasPartes.forEach(batidaParte => {
-                if (batidaParte.includes("s")) {
-                    batidaParteIntervalo = Number(batidaParte.replace("s", ""))                    
-                } else {
-                    batidaParteIntervalo = 1
-                }
+        )
+        ];
 
-                timeout += batidaParteIntervalo * 1000 / velocidade
-                
-                if (batidaParte == "dom") {
-                    idsReproducao.push(setTimeout(tocarDom, timeout))
-                    batidaParteIntervalo = 1
-                } else if (batidaParte == "tchi") {
-                    idsReproducao.push(setTimeout(tocarTchi, timeout))
-                    batidaParteIntervalo = 1
-                } else if (batidaParte == "dim") {
-                    idsReproducao.push(setTimeout(tocarDim, timeout))
-                    batidaParteIntervalo = 1
-                } else if (batidaParte == "domchi") {
-                    idsReproducao.push(setTimeout(tocarDomChi, timeout))
-                    batidaParteIntervalo = 1
-                }
-            })
-            if (document.querySelector("#repetir").checked) {
-                idsReproducao.push(setTimeout(() => tocar(batidas), timeout))
-            }            
-        }
+    const shareData = {
+        title: "Berimbau",
+        text: "Aprenda a tocar Berimbau",
+        url: "https://franklindeoliveira.github.io/berimbau",
+        files: filesArray
+    };
+    await navigator.share(shareData);
+})
 
-        function parar() {
-            idsReproducao.forEach(idReproducao => {
-                clearTimeout(idReproducao)
-            })
-            idsReproducao = []
-        }
+document.querySelectorAll(".play-audio").forEach(audioPlay => audioPlay.addEventListener("click", playAudio))
 
-        function tocarTchi() {
-            new Audio("audio/tchi.mp3").play()
-        }
+function playAudio(event) {
+    console.log(`tocando audio ...`)
+    console.log(event.currentTarget)
+    let play = event.currentTarget.querySelector("i")
+    console.log(play.textContent)
 
-        function tocarDomChi() {
-            new Audio("audio/domchi.mp3").play()
-        }
+    let playState = play.textContent
 
-        function tocarDim() {
-            new Audio("audio/dim.mp3").play()
-        }
+    document.querySelectorAll(".play-audio").forEach(audioPlay => {
+        audioPlay.querySelector("i").textContent = "stop"
+        audioPlay.classList.remove("playing")        
+    })
 
-        function tocarDom() {
-            new Audio("audio/dom.mp3").play()
-        }
+    event.currentTarget.classList.add("playing")
 
-        document.querySelector("#compartilhar").addEventListener("click", async function(event) {
-            const response = await fetch('https://media.istockphoto.com/id/924098198/pt/vetorial/isolated-colorful-decorative-ornate-berimbau-with-caxixi-baqueta-and-dobrao-on-white.jpg?s=612x612&w=0&k=20&c=Tw57MSFN9X2-f-1PFnztcP2V9PLvO2HtlC36nFEzEPU=');
-            const blob = await response.blob();
-            const filesArray = [
-                new File(
-                  [blob],
-                  'berimbau.jpg',
-                  {
-                    type: "image/jpeg"
-                  }
-               )
-              ];
-
-            const shareData = {
-                title: "Berimbau",
-                text: "Aprenda a tocar Berimbau",
-                url: "https://franklindeoliveira.github.io/berimbau",
-                files: filesArray
-            };
-            await navigator.share(shareData);
-        })
+    if (playState == "stop") {
+        play.textContent = "play_arrow"
+    } else {
+        play.textContent = "stop"
+        event.currentTarget.classList.remove("playing")
+    }
+}
